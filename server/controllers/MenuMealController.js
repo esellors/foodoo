8c8953e04cee66241668d76063741ec424d3da36
Eric <github@esellors.com>
2019-05-24 20:28:32 -0500

    fixed issue where selecting items turned them into array and messed with moving them from menu to meal and vice versa

--- server/controllers/MenuMealController.js
+++ server/controllers/MenuMealController.js
@@ -20,12 +20,16 @@ let getItems = (req, res) => {
 let moveItems = (req, res) => {
    const {section, category, item, destinationSection} = req.params;
    let index = menuMeal[section][category].findIndex(arrayItem => arrayItem === item);
-   let tgtItem = menuMeal[section][category].splice(index, 1)
+   let tgtItem = menuMeal[section][category].splice(index, 1).toString();
+   
+   console.log(tgtItem)
+
    menuMeal[destinationSection][category].push(tgtItem);
    res.json(menuMeal);
+
 }
 
-let deleteItems = (req, res) => {
+let deleteItem = (req, res) => {
    const {category, item} = req.params;
    let index = menuMeal.menu[category].findIndex(arrayItem => arrayItem === item);
    menuMeal.menu[category].splice([index], 1);
@@ -41,5 +45,5 @@ let deleteItems = (req, res) => {
 module.exports = {
    getItems,
    moveItems,
-   deleteItems
+   deleteItem
 };
--- server/index.js
+++ server/index.js
@@ -11,17 +11,9 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.get("/api/menumeal", mmc.getItems);
+// app.post("/api/menumeal", mmc.addIt)
 app.put("/api/menumeal/:section/:category/:item/:destinationSection", mmc.moveItems);
-app.delete("/api/menumeal/:category/:item", mmc.deleteItems);
-
-// app.post
-
-
-
-
-
-
-
+app.delete("/api/menumeal/:category/:item", mmc.deleteItem);
 
 app.listen(SERVER_PORT, () => {
    console.log(`Chilling to Lo-fi ${8080} fm`);
