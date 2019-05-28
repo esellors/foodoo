9f00fed49e4e4fda5f76e4c4950f219ee712a132
Eric <github@esellors.com>
2019-05-28 01:16:29 -0500

    add style to title and tweaked delete button for add-items in menu section

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -26,7 +26,7 @@ function AddItem(props) {
       <form className="add_to_menu_form hide_element" id={props.id} onBlur={e => cancelAddItem(e)}>
          <input className="add_item" placeholder="enter item..."/>
          <input type="submit" className="add_btn" onClick={e => addItemHandler(e)} hidden />
-         <span id="cancel_add_item">X</span>
+         <span id="cancel_add_item">x</span>
       </form>
    );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
    render() {
       return (
          <header>
-            <h2>foodoo</h2>
+            <h2>---------------<span id="site_title">foodoo</span></h2>
             <h4>your daily meal planner</h4>
          </header>
       )
