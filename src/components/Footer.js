c11ecd200c9384acd67ca1cc69f216969f066474
Eric <github@esellors.com>
2019-05-30 21:42:24 -0500

    added state to header and footer components for extra points

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Footer(props) {
-   return (
-      <div id="footer">
-         <small>&copy;2019 Eric Sellors</small>
-      </div>
-   );   
+class Footer extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         footerTxt: 'Made by Eric Sellors'
+      }
+   }
+   render() {
+      return (
+         <div id="footer">
+            <small>{this.state.footerTxt}</small>
+         </div>
+      );   
+   }
 }
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 
 class Header extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         headerTxt: 'foodoo'
+      }
+   }
    render() {
       return (
          <header>
-            <h2>---------------<span id="site_title">foodoo</span></h2>
+            <h2>---------------<span id="site_title">{this.state.headerTxt}</span></h2>
             <h4>your daily meal planner</h4>
          </header>
       )
